14ddcdeea0bda55f4a2c548d3f8106bfd1a60915
zootrash <zootrash@users.noreply.github.com>
2016-06-26 13:01:47 -0700

    finished with ll.

--- ll/ll.c
+++ ll/ll.c
@@ -2,11 +2,52 @@
 #include <stdlib.h>
 
 /* Linked Lists [Singly] */
+/* If stms ensures that mem. was properly allocated
+ * before traversing the list.
+ *
+ * While loop cont. as long as there is a ptr. to
+ * the next node; conductor goes all the way to the
+ * end of the ll.
+ *
+ * The conductor just moves along, changing what it
+ * points to by getting the addr. of conductor->next.
+ *
+ * At the end of the code, just uses a quick check to
+ * ensure we're not OOM, then conductor traverses
+ * one more ele. and makes sure its pointer is set to
+ * 0 so that the list has an end. 
+ *
+ *
+ *
+ */
 
 struct node {
 	int x;
 	struct node *next;
 };
+
+/* printll dn. print last ele. so you have to do so. */
+void print_ll(struct node *root) {
+	struct node *cond;
+	cond = root; 
+	if(cond != 0) {
+		while(cond->next != 0) {
+			printf("%d\n", cond->x);
+			cond = cond->next;
+		}
+		printf("%d\n", cond->x);
+	}
+}
+
+/* alt dn. req. last print and if stmt like printll. */
+void altprint_ll(struct node *root) {
+	struct node *cond;
+	cond = root;
+	while(cond != NULL) {
+		printf("%d\n", cond->x);
+		cond = cond->next;
+	}
+}
 
 /* main */
 int main() {
@@ -24,6 +65,21 @@ int main() {
 			conductor = conductor->next;
 		}
 	}
+
+	conductor->next = malloc(sizeof(struct node));
+
+	conductor = conductor->next;
+
+	if(conductor == 0) {
+		printf("Out of memory!\n");
+		return 0;
+	}
+
+	conductor->next = 0;
+	conductor->x = 42;
+
+	//print_ll(root;)
+	//altprint_ll(root);
 	
 	return 0;
 }
